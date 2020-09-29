2f5e952a777918c7e7d93e1051d8f4655bac8124
akhilkumarpilli <akhilkumar7947@gmail.com>
2020-09-29 17:10:27 +0530

    Fix few address format change issues in other modules

--- provider/cmd/serviceStatus.go
+++ provider/cmd/serviceStatus.go
@@ -42,7 +42,7 @@ func doServiceStatus(cmd *cobra.Command) error {
 	}
 
 	pclient := pmodule.AppModuleBasic{}.GetQueryClient(cctx)
-	res, err := pclient.Provider(context.Background(), &ptypes.QueryProviderRequest{Owner: addr})
+	res, err := pclient.Provider(context.Background(), &ptypes.QueryProviderRequest{Owner: addr.String()})
 	if err != nil {
 		return err
 	}
